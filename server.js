1b38f4d696619ee85e3c9ff2f5b25adbfda0cb9f
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-11-22 14:13:59 +1030

    try again

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-let express = require("express");
-let routes = require("./controllers/burgers_controller.js");
-let exphbs = require("express-handlebars");
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+
+const app = express();
 
 const PORT = process.env.PORT || 8080;
-
-let app = express();
-
-app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json);
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+
+app.use("public", express.static(path.join(__dirname, "public")));
+
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
